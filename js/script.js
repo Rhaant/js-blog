ec275c2eb253e951a9af31131f202aa122b67f9d
Leszek <leszek.synowiec@gmail.com>
2019-12-12 20:32:20 +0100

    Add complete app, implement remove function to remove classes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 'use strict';
 
-function titleClickHandler(){
-  console.log('Link was clicked!');
-  console.log(event);
+function titleClickHandler(){ 
+    event.preventDefault();
+    remove();
+    this.classList.add('active')
+    let atribute = this.getAttribute('href');
+    let targetAtribute = document.querySelector(atribute);
+    targetAtribute.classList.add('active');
+    console.log(targetAtribute);
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -11,15 +16,20 @@ for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
+remove();
 /* remove all active classes from all article links */
+function remove () {
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
 
+/* remove all active classes from article posts */
 const activeArticles = document.querySelectorAll('.posts article.active');
 
 for (let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
+}
+
 }
