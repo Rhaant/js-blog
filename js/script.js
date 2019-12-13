0f38a150181cf012f93f9db676b93ba6b061b8cc
Leszek <leszek.synowiec@gmail.com>
2019-12-13 12:35:27 +0100

    fixed problem with opening article

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 'use strict';
+/* FUNKCJE */
+
+function remove () {
+    /* remove all active classes from all article links */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+    
+    /* remove all active classes from article posts */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    
+    for (let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+    
+    }
 
 function titleClickHandler(){ 
     event.preventDefault();
@@ -10,26 +28,23 @@ function titleClickHandler(){
     console.log(targetAtribute);
 }
 
+/* PROGRAM */
+
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
-remove();
-/* remove all active classes from all article links */
-function remove () {
-const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-}
+// const articleNames = document.querySelectorAll('.titles a');
 
-/* remove all active classes from article posts */
-const activeArticles = document.querySelectorAll('.posts article.active');
+// for (let articleName of articleNames){
+// }
 
-for (let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-}
+// // const articleIds = document.querySelectorAll('post');
 
-}
+// // for (let articleId of articleIds) {
+    
+// }
+
