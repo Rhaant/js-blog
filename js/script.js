1a59da3a186d4da5f09c418eb58a97f8363e030d
Leszek <leszek.synowiec@gmail.com>
2019-12-23 23:15:53 +0100

    add tag cloud

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* eslint-disable linebreak-style */
 'use strict';
+
+let fontSize = 0;
 /* FUNKCJE */
 
 function remove () {
@@ -26,7 +28,7 @@ function titleClickHandler(){
   let atribute = this.getAttribute('href');
   let targetAtribute = document.querySelector(atribute);
   targetAtribute.classList.add('active');
-  console.log(targetAtribute);
+//   console.log(targetAtribute);
 }
 
 function removePostLinks () {
@@ -66,7 +68,7 @@ function generateTags() {
       const tag = document.createElement('li');
       tag.innerHTML = `<a href=#tag-${dataTag}><span>${dataTag}&nbsp;</span></a>`;
       article.querySelector('.list').appendChild(tag);
-      console.log(dataTag);
+    //   console.log(dataTag);
     }
   }
 }
@@ -90,15 +92,15 @@ function tagClickHandler(){
     // console.log(tag);
   }
   const activeTags = document.querySelectorAll(`article a[href="${href}"]`);
-  console.log(activeTags);
+  //   console.log(activeTags);
   for (let activeTag of activeTags){
     const hrefTag = activeTag.closest('article').getAttribute('id');
     const postLinkName = activeTag.closest('article').querySelector('.post-title').innerHTML;
     const newTag = document.createElement('li');
     newTag.innerHTML = `<a href=#${hrefTag}><span>${postLinkName}</span</a>`;
     document.querySelector('.wrapper ul').appendChild(newTag);
-    console.log(hrefTag);
-    console.log(postLinkName);
+    // console.log(hrefTag);
+    // console.log(postLinkName);
 
   }
 
@@ -110,12 +112,16 @@ function addClickListenersToTags(){
   for (let link of links){
     link.addEventListener('click', tagClickHandler);
   }
+  const cloudLinks = document.querySelectorAll('aside .tags a');
+  for (let cloudLink of cloudLinks){
+    cloudLink.addEventListener('click', tagClickHandler);
+  }
 }
 
 function generateAuthors() {
 
   const authors = document.querySelectorAll('.authors li');
-  console.log(authors);
+  //   console.log(authors);
   for (let author of authors){
     author.remove();
   }
@@ -129,7 +135,7 @@ function generateAuthors() {
       authorsList.push(authorData);
       const authorTag = document.createElement('li');
       authorTag.innerHTML = `<a href="${authorData}"><span class="author-name">${authorData}</span></a>`;
-      console.log(authorData);
+      //   console.log(authorData);
       document.querySelector('ul.authors').appendChild(authorTag);  
     }
 
@@ -141,14 +147,14 @@ function generateAuthors() {
 function authorsClickHandler(){
   event.preventDefault();
   removePostLinks();
-  console.log(this);
+  //   console.log(this);
   const href = this.getAttribute('href');
-  console.log(href);
+  //   console.log(href);
   const articleAuthors = document.querySelectorAll(`[data-author="${href}"]`);
-  console.log(articleAuthors);
+  //   console.log(articleAuthors);
   for (let articleAuthor of articleAuthors){
     const articleID = articleAuthor.getAttribute('id');
-    console.log(articleID);
+    // console.log(articleID);
     const tagLink = document.createElement('li');
     tagLink.innerHTML = `<a href=#${articleID}><span>${articleID}</span</a>`;
     document.querySelector('.titles').appendChild(tagLink);
@@ -162,10 +168,44 @@ function generateAuthorsLink() {
   }
 }
 
+function generateTagsLinks() {
+  const articles = document.querySelectorAll('article');
+  const tagsCounter = {};
+  let listOfTags = [];
+  for (let article of articles){
+    const articleTags = article.getAttribute('data-tags').split(' ');
+    for (let articleTag of articleTags){
+      listOfTags.push(articleTag);
+    }
+    // console.log(listOfTags);
+  }
+  listOfTags.forEach(function(tag){
+    tagsCounter[tag] = (tagsCounter[tag] || 0)+1;
+  });
+  //   console.log(tagsCounter);
+  for (let key in tagsCounter){
+    const linkHtml = document.createElement('li');
+    linkHtml.innerHTML = `<a href=#tag-${key}><span>${key}&nbsp;</span></a>`;
+    fontSize = Math.floor((tagsCounter[key]+1)*0.5);
+    linkHtml.classList.add(`tag-size-${fontSize}`);
+    document.querySelector(`aside .tags`).appendChild(linkHtml);
+    // console.log(key);
+  }
+
+  let value = Object.keys(tagsCounter).map((key)=>{return tagsCounter[key];});
+  let min = Math.min.apply(null, value);
+  let max = Math.max.apply(null, value);
+  console.log(min, max);
+}
+
+
+
+
 /* PROGRAM */
 
 generatePostLinks();
 generateTags();
+generateTagsLinks();
 addClickListenersToTags();
 generateAuthors();
 generateAuthorsLink();
